0b40a3ed49de9a387543b4d46e6988449984fe55
Profe Mario <mariocaresca@santotomas.cl>
2025-05-22 19:50:16 -0400

    Agrega endpoint fake de usuario

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import userRouter from "../src/routers/users.route.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/usuarios', userRouter);
 
 app.get('/', (request, response) => {
     response.send("Servicio funcionando correctamente");
